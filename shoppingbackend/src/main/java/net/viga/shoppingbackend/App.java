c4b166536e13170415a528fb2dfeed2437b76fbd
vigasini <svigasini@gmail.com>
2018-03-06 22:59:30 +0530

    my project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/viga/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.viga.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
